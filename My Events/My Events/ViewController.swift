bc5a2befb9b00f9d0d9f19f70a3b6b3e0e92e0d0
yazzgul <yazgul.khasanshina@gmail.com>
2024-03-21 21:38:22 +0300

    add swiftgen in pods

--- My Events/My Events/ViewController.swift
+++ My Events/My Events/ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
